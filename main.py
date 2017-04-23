94f21b5d3d4fc97ad4e7f1a4038b9751e36e2715
Mitesh Kumar Savita <miteshkumarsavita@gmail.com>
2017-04-23 16:31:04 -0400

    history viz done

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def hdi_history(filename):
     avg = [0] * 7   # init list of avg HDI values for all countries.
     count = [0] * 7
     years = []
+    usa_hdi = []
 
     for year in df.keys()[2:]:
         years.append(year.split(" ")[-1:][0])
 
     for i in range(0, len(df)):
+        # save row for usa.
+        if df.ix[i][1] == 'United States':
+            usa_hdi = df.ix[i].tolist()[2:]
         for j in range(0, 7):
             try:
                 avg[j] += float(df.ix[i][j+2])
@@ -20,17 +24,44 @@ def hdi_history(filename):
             except ValueError:
                 continue    # skipping junk values
 
+    # calculating avg HDI for each year.
     for i in range(0, 7):
         avg[i] /= count[i]
+        avg[i] = str(avg[i])
 
-    print years
+    # converting years to integer values
+    for i in range(0, len(years)):
+        years[i] = years[i][1:-1]
+        print years[i]
+
+    for i in range(0, len(usa_hdi)):
+        usa_hdi[i] = str(usa_hdi[i])
+    print usa_hdi
     print avg
 
-    new_df = pd.DataFrame(columns=years)
-    new_df.loc[0] = avg
+
+    # converting values to float
+    #usa_hdi = map(float, usa_hdi)
+    print years
+
+
+
+    columns = ['symbol', 'date', 'hdi']
+    symbols = ['world', 'usa']
+
+    new_df = pd.DataFrame(columns=columns)
+
+    # fill the new data frame with values
+    count = 0
+    for i in range(0, len(avg)):
+        new_df.loc[count] = ['World', years[i], avg[i]]
+        count += 1
+    for i in range(0, len(avg)):
+        new_df.loc[count] = ['USA', years[i], usa_hdi[i]]
+        count += 1
 
     print new_df
-    new_df.to_csv('history.csv')
+    new_df.to_csv('history.csv', float_format='%.0f')
 
 if __name__ == '__main__':
     '''HDI over the years'''
