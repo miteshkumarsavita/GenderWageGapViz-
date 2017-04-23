537a3ed4f7a659bc487229ee980bac8e80a67675
Mitesh Kumar Savita <miteshkumarsavita@gmail.com>
2017-04-23 14:27:03 -0400

    history data complete, need to visualize

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import sys
 import pandas as pd
 import csv
 
+
 def hdi_history(filename):
     df = pd.read_csv(filename)
     avg = [0] * 7   # init list of avg HDI values for all countries.
     count = [0] * 7
+    years = []
+
+    for year in df.keys()[2:]:
+        years.append(year.split(" ")[-1:][0])
 
     for i in range(0, len(df)):
         for j in range(0, 7):
@@ -18,7 +23,14 @@ def hdi_history(filename):
     for i in range(0, 7):
         avg[i] /= count[i]
 
-    
+    print years
+    print avg
+
+    new_df = pd.DataFrame(columns=years)
+    new_df.loc[0] = avg
+
+    print new_df
+    new_df.to_csv('history.csv')
 
 if __name__ == '__main__':
     '''HDI over the years'''
